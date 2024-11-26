736c2f19afe3bdf3fd94a18283b90cd8daec6f2d
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-11-25 22:02:09 -0500

    doc: add documentation to calculate square root

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 def calculate_square_root(number):  
+    """Calculates the sqaure root of a given number
+
+    Args:
+        number (float): the  number to calculate the sqaure root
+
+    Returns:
+        float: the sqaure root of the number 
+    """
     return number ** 0.5
 
 def test_calculate_square_root():
