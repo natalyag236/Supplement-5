2ce9affb78a8a3ef351bd2386ed6b7948f5dd0fc
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-11-25 21:59:30 -0500

    feat:implement sqaure root calcuation function

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-def test_calculate_sqaure_root():
-    assert calculate_sqaure_root(9) == 3
+def calculate_square_root(number):  
+    return number ** 0.5
+
+def test_calculate_square_root():
+    assert calculate_square_root(9) == 3
